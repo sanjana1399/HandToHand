5ae35ae91ebea2f64cdd574ce6825b44e2aed592
Niharika Maruvanahalli Suresh <43085656+nih326@users.noreply.github.com>
2025-02-08 23:52:10 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import CommunitySharing from "./components/CommunitySharing";
+import SpecialRequests from "./components/SpecialRequests";
+
 function App() {
   return (
-    <div>
-      <CommunitySharing />
-    </div>
+    <Routes>
+      <Route path="/" element={<CommunitySharing />} />
+      <Route path="/special-requests" element={<SpecialRequests />} />
+    </Routes>
   );
 }
 
